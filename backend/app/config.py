0a570ecc48ba5c3d61426a667d7474df66d9fd28
Siddesh Lohkare <118656715+Siddesh3108@users.noreply.github.com>
2026-04-24 13:07:37 +0530

    configuration settings

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,8 +1,10 @@
 import os
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'a-very-secret-key-that-you-should-change')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@db/timetable_db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CELERY_BROKER_URL = 'redis://redis:6379/0'
-    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
-    REMEMBER_COOKIE_DURATION = 31536000 # 365 days
+    CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://redis:6379/0')
+    CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
+    REMEMBER_COOKIE_DURATION = 31536000  # 365 days
+
